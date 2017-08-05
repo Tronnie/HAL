7203b4e31c93a10c29a77860a905cc37820eec18
G I <tronnie@Gs-MacBook-Pro.local>
2017-08-05 16:34:52 -0700

    last push before work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,51 @@
-
+//create an array for the computer to choose.
   var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+//declare variables that will be updated at user plays
   var wins = 0;
   var losses = 0;
   var guesses = [];
-  var chances = 0; //don't know how to get this to work
+  var chances = 2; //don't know how to get this to work
 
-
+//recognize when the player presses a key
   document.onkeyup = function(event) {
     var player = event.key;
-      if
+      if //make sure they only press letters because that's what's in the array
       (!letters.includes(player)){
         alert("pick letters only");
         return;
       };
 
-    if (chances = 0){
-      alert("Yooour score is: ");
-      return;
-    }
+      if (chances = 0){ //was hoping to limit number of plays
+      alert("Your score is: "); // this is not working. not sure where it goes.
+      ;
+    };
 
-
+//make the computer choices from array random
     var computer = letters[Math.floor(Math.random()*letters.length)];
-
+//increase the wins score if player and computer have same letter. record letter the player typed.
     if(player ===computer){
       wins++;
       guesses+=player;
     }
+//increase the losses if player guess does not match computer guess.  record the letter the player typed.
     if(player !== computer){
       losses+=1;
       guesses+=player;
-      chances;
-    }
 
+    }
+//update the variables in the html to show the player results
     var html =
       "<p> You chose: " + player + "</p>"
       +
       "<p> I chose: " + computer + "</p>"
       +
       "<p> Guesses:" + guesses + "</p>"+
-      "<p> Plays left:"+ --chances +"</p>" +
+      "<p> Plays left:" + -chances +"</p>" + //doesn't work :(
       "<p> Wins: " + wins + "</p>"
       +
       "<p> Losses: " + losses + "</p>"
 
       ;
-
+//display the updated results.
     document.querySelector("#game").innerHTML = html;
   };
